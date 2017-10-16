249849232f3efbdc3f461feb9c195d2d13ca8a6e
zhangran1 <1565461180@qq.com>
2017-10-16 11:06:16 +0800

    add

--- 10--11/websitel/2.about.php
+++
@@ -1,5 +0,0 @@
-<?php include "./include/header.php"
-
- ?>
- <h1>About</h1>
- <?php  include "./include/footer.php" ?>
--- 10--11/websitel/3.contact(联系).php
+++
@@ -1,5 +0,0 @@
-<?php include "./include/header.php"
-
- ?>
- <h1>Contact</h1>
- <?php  include "./include/footer.php" ?>
